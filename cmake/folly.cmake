852a9d593b2fedcad5961bafae8e63e7543367c8
Rahul Kushwaha <rahul.kushwaha@gmail.com>
2023-03-29 22:00:51 -0700

    Use variable for openSSL location

--- cmake/folly.cmake
+++ cmake/folly.cmake
@@ -1,11 +1,10 @@
 set(CMAKE_CXX_FLAGS "-fcoroutines-ts")
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
 include(FetchContent)
-set(ENV{OPENSSL_ROOT_DIR} "/Users/rahulkushwaha/projects/replicated_counter/cmake-build-debug/openssl")
 FetchContent_Declare(
         folly
         GIT_REPOSITORY https://github.com/facebook/folly.git
-        GIT_TAG v2023.03.27.00 # release-1.10.0
+        GIT_TAG v2023.03.27.00
 )
 
-FETCHCONTENT_MAKEAVAILABLE(folly)
+fetchcontent_makeavailable(folly)
