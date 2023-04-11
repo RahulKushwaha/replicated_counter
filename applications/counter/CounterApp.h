01318a8db5b6fbe72a308a4aa23f7c41df7c4492
Aman Mahajan <amanmahajan@Amans-MacBook-Air.local>
2023-04-10 23:10:06 -0700

    Implementing BatchEndpoint

--- applications/counter/CounterApp.h
+++ applications/counter/CounterApp.h
@@ -16,21 +16,47 @@ using namespace rk::projects::durable_log;
 
 class CounterApp {
  public:
+  struct IncrOperation {
+    std::string key;
+    std::int64_t incrBy;
+  };
+
+  struct DecrOperation {
+    std::string key;
+    std::int64_t incrBy;
+  };
+
+  struct CounterValue {
+    std::string key;
+    std::int64_t val;
+  };
+  using CounterValues = std::vector<CounterValue>;
+
+  using Operation = std::variant<IncrOperation, DecrOperation>;
+
   explicit CounterApp(std::shared_ptr<VirtualLog> virtualLog);
 
-  folly::coro::Task<std::int64_t> incrementAndGet(std::string key, std::int64_t incrBy);
-  folly::coro::Task<std::int64_t> decrementAndGet(std::string key, std::int64_t decrBy);
-  folly::coro::Task<std::int64_t>  getValue(std::string key);
+  folly::coro::Task<std::int64_t>
+  incrementAndGet(std::string key, std::int64_t incrBy);
+  folly::coro::Task<std::int64_t>
+  decrementAndGet(std::string key, std::int64_t decrBy);
+  folly::coro::Task<std::int64_t> getValue(std::string key);
+  folly::coro::Task<std::vector<CounterApp::CounterValue>>
+  batchUptate(std::vector<Operation> operations);
 
  private:
   static std::string
   serialize(std::string key,
             std::int64_t val,
             CounterLogEntry_CommandType commandType);
-  static CounterLogEntry deserialize(std::string payload);
+  static CounterLogEnteries deserialize(const std::string& payload);
 
-  void apply(const CounterLogEntry &counterLogEntry);
-  void sync(LogId to);
+  static std::string
+  serialize(const std::vector<Operation>& operations);
+
+  void apply(const CounterLogEnteries &counterLogEnteries);
+  std::vector<CounterValue> sync(LogId to);
+
 
  private:
   std::shared_ptr<VirtualLog> virtualLog_;
@@ -38,6 +64,7 @@ class CounterApp {
   std::unique_ptr<std::mutex> mtx_;
 
   std::unordered_map<std::string, std::atomic_int64_t> lookup_;
+  CounterLogEnteries applyLogEntries(LogId logIdToApply);
 };
 
 }
