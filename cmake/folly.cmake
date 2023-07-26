061d40c0dbf0963055ae28b859ff8b5cc95316cc
Rahul Kushwaha <rahul.kushwaha@gmail.com>
2023-07-25 21:03:13 -0400

    Move to cpp 20

--- cmake/folly.cmake
+++ cmake/folly.cmake
@@ -1,10 +1,10 @@
 set(CMAKE_CXX_FLAGS "-fcoroutines-ts")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
+#set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++ -lc++abi")
 include(FetchContent)
 FetchContent_Declare(
         folly
         GIT_REPOSITORY https://github.com/facebook/folly.git
-        GIT_TAG v2023.05.22.00
+        GIT_TAG v2023.07.24.00
 )
 
 fetchcontent_makeavailable(folly)
