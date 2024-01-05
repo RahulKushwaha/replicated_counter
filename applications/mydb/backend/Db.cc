46bf1f182717e1cd341a434a8be7fd92499c8953
Rahul Kushwaha <rahul.kushwaha@gmail.com>
2024-01-04 17:44:32 -0800

    Add Table Operation

--- applications/mydb/backend/Db.cc
+++ applications/mydb/backend/Db.cc
@@ -13,7 +13,9 @@ namespace rk::projects::mydb {
 Db::Db(std::shared_ptr<SchemaStore> schemaStore,
        std::shared_ptr<RocksReaderWriter> rocks)
     : schemaStore_{std::move(schemaStore)},
-      queryExecutor_{std::make_shared<QueryExecutor>(std::move(rocks))} {}
+      queryExecutor_{std::make_shared<QueryExecutor>(std::move(rocks))},
+      dbIds_{0},
+      tableIds_{0} {}
 
 google::protobuf::Empty Db::addDatabase(
     const client::AddDatabaseRequest* request) {
@@ -22,24 +24,47 @@ google::protobuf::Empty Db::addDatabase(
 
   auto tableSchema =
       std::make_shared<TableSchema>(std::make_shared<Table>(table.value()));
+  const auto dbId = dbIds_++;
 
-  auto internalTable = Transformer::from(*request, tableSchema);
+  auto internalTable = Transformer::from(*request, tableSchema, dbId);
 
   queryExecutor_->insert(internalTable);
+
+  internal::Database database{};
+  database.set_id(dbId);
+  database.set_name(request->database().name().name());
+
+  const auto result =
+      schemaStore_->registerDb(request->database().name().name(), database);
+  assert(result && "unable to register schema");
 
   return google::protobuf::Empty{};
 }
 
 google::protobuf::Empty Db::addTable(const client::AddTableRequest* request) {
   const auto db = request->database();
-  auto table = schemaStore_->getTable(db.name().name(), "meta_db_table");
+  auto table = schemaStore_->getTable("meta", "meta_db_table");
   assert(table.has_value() && "table not found");
+
+  // make sure the database in which the table is being inserted exists
+  auto database = schemaStore_->getDatabase(request->database().name().name());
+
+  if (!database.has_value()) {
+    throw std::runtime_error{fmt::format("{} named database does not exist",
+                                         request->database().name().name())};
+  }
+
+  const auto tableId = tableIds_++;
 
   auto tableSchema =
       std::make_shared<TableSchema>(std::make_shared<Table>(table.value()));
-  auto internalTable = Transformer::from(*request, tableSchema);
+  auto internalTable = Transformer::from(*request, tableSchema, tableId);
 
   queryExecutor_->insert(internalTable);
+
+  schemaStore_->registerTable(
+      request->database().name().name(), request->table().name().name(),
+      Transformer::from(request->table(), database.value().id(), tableId));
 
   return google::protobuf::Empty{};
 }
@@ -61,6 +86,43 @@ client::AddRowResponse Db::addRow(const client::AddRowRequest* request) {
 
 client::UpdateRowResponse Db::updateRow(
     const client::UpdateRowRequest* request) {
+  auto table = schemaStore_->getTable(request->database_name().name(),
+                                      request->table_name().name());
+  assert(table.has_value() && "table not found");
+
+  auto tableSchema =
+      std::make_shared<TableSchema>(std::make_shared<Table>(table.value()));
+
+  QueryPlan queryPlan{
+      .operation = QueryOperation::TableScan,
+      .schema = tableSchema,
+      .condition = request->condition(),
+  };
+
+  QueryPlanner queryPlanner{queryPlan, queryExecutor_};
+  auto executableQueryPlan =
+      queryPlanner.plan(InternalTable{.schema = tableSchema});
+  auto result = queryPlanner.execute(executableQueryPlan);
+
+  if (result.table->num_rows() != 1) {
+    throw std::runtime_error{"row did not match the condition"};
+  }
+
+  auto internalTable = Transformer::from(*request, tableSchema);
+  UpdateOptions updateOptions{};
+  switch (request->update_type()) {
+    case client::UpdateRowRequest_UPDATE_TYPE_REPLACE:
+      updateOptions = UpdateOptions::REPLACE;
+      break;
+    case client::UpdateRowRequest_UPDATE_TYPE_MERGE:
+      updateOptions = UpdateOptions::MERGE;
+      break;
+    default:
+      assert(false && "invalid update type");
+  }
+
+  queryExecutor_->update(internalTable, updateOptions);
+
   return client::UpdateRowResponse{};
 }
 
